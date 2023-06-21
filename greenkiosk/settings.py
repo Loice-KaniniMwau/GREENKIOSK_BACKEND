8d97b6acc6dd939a73965d0b5063ab1d895da515
Loice-KaniniMwau <mwauloice@gmail.com>
2023-06-21 08:34:22 +0300

    greenkiosk backend

--- greenkiosk/settings.py
+++ greenkiosk/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'customer',
     'shoppingcart',
     'delivery',
+    'payment',
 ]
 
 MIDDLEWARE = [
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    PAYMENT_METHODS=(("mpesa","Mpesa"),("cash","Cash"),)
+    PAYMENT_STATUS=(("approved","Approved"),("pending","Pending"),("cancelled","Canclled"),)
+    # name=models.CharField(max_length=32)
+    # userid=models.CharField(max_length=32)
+    paymentmethod=models.CharField(max_length=32,choices=PAYMENT_METHODS)
+    paymentstatus=models.CharField(max_length=32,choices=PAYMENT_STATUS)
+    datepaid=models.DateTimeField(auto_now_add=True)
